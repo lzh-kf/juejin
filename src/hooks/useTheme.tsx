71c669ad007f0845d81f53be7f06aac9b8adefa5
lizhenhui <15088792512@163.com>
2024-07-26 08:39:08 +0800

    fix:修复主题切换错乱

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react'
 import { Appearance } from 'react-native'
+const colorScheme = Appearance.getColorScheme()
 const useTheme = () => {
-  const colorScheme = Appearance.getColorScheme()
   const isDark = useMemo(() => colorScheme === 'dark', [colorScheme])
   const backgroundColor = useMemo(() => (isDark ? '#222222' : '#fff'), [isDark])
   const color = useMemo(() => (isDark ? '#fff' : '#000'), [isDark])
